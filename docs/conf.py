70c64b67887fe7398b0a0b586112404dc9b30477
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2024-06-20 16:50:36 +0200

    Updated main images and logo

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ html_context = {
 html_static_path = ["./_static"]
 html_css_files = ["css/custom.css"]
 html_logo = "./_static/logo.png"
-html_favicon = "./_static/logo.ico"
+html_favicon = "./_static/logo.ico"  # Icon for browser tab
 
 # -- Extension configuration -------------------------------------------------
 intersphinx_mapping = {
